7a8d6e9aca057d0958104a92a63c61a2ee348ea6
SOUFIANE <soufiane@SOUFIANEs-MacBook-Pro.local>
2021-07-10 19:29:30 +0400

    redesign login page

--- src/Security/BlogAuthenticator.php
+++ src/Security/BlogAuthenticator.php
@@ -49,10 +49,7 @@ class BlogAuthenticator extends AbstractLoginFormAuthenticator
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
         }
-
-        // For example:
-        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('actualites'));
     }
 
     protected function getLoginUrl(Request $request): string
