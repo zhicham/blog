139a81a1f49f97136ef560f4cf68fc72a610da3e
SOUFIANE <soufiane@SOUFIANEs-MacBook-Pro.local>
2021-07-10 20:17:41 +0400

    Articles + Pagination

--- src/Controller/ActualitesController.php
+++ src/Controller/ActualitesController.php
@@ -5,16 +5,31 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Articles;
+
+
+use Symfony\Component\HttpFoundation\Request; // Nous avons besoin d'accéder à la requête pour obtenir le numéro de page
+use Knp\Component\Pager\PaginatorInterface; // Nous appelons le bundle KNP Paginator
+
 
 class ActualitesController extends AbstractController
 {
     /**
      * @Route("/actualites", name="actualites")
      */
-    public function index(): Response
+    public function index(Request $request, PaginatorInterface $paginator): Response
     {
+        $donnees = $this->getDoctrine()->getRepository(Articles::class)->findBy([],['id' => 'desc']);
+        $articles = $paginator->paginate(
+            $donnees, // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            6 // Nombre de résultats par page
+        );
+        
+        
         return $this->render('actualites/index.html.twig', [
             'controller_name' => 'ActualitesController',
+            'articles' => $articles 
         ]);
     }
 }
