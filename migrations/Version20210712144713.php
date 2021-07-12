4d2a99140c1a218da67461d09f8a94c499609ad8
mac <tchikh3@gmail.com>
2021-07-12 15:49:10 +0100

    ui login register

--- migrations/Version20210712144713.php
+++ migrations/Version20210712144713.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210710145819 extends AbstractMigration
+final class Version20210712144713 extends AbstractMigration
 {
     public function getDescription(): string
     {
